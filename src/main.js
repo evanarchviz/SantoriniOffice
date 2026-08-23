f0912c02baacf916ded0d90025ad9ea183cfa23e
evanarchviz <evanarchviz@gmail.com>
2026-08-23 18:09:22 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ const normalCameraFov = 75;
 const zoomCameraFov = 25;
 const zoomLerpSpeed = 14;
 const SPAWN = new THREE.Vector3(-1.27302, 1.41303, -2.35058);
-const SPAWN_YAW = THREE.MathUtils.degToRad(90);
+const SPAWN_YAW = THREE.MathUtils.degToRad(-39.7486);
 
 const ui = {
     loadingScreen: document.getElementById("loadingScreen"),
